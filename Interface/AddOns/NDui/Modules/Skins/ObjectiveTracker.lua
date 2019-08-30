7c046e4f85f48d7b7307cda83389ce1b399e7682
siweia <siwei1990@gmail.com>
2019-08-30 22:19:25 +0800

    Update look for quest tracker

--- Interface/AddOns/NDui/Modules/Skins/ObjectiveTracker.lua
+++ Interface/AddOns/NDui/Modules/Skins/ObjectiveTracker.lua
@@ -2,6 +2,7 @@ local _, ns = ...
 local B, C, L, DB = unpack(ns)
 local S = B:GetModule("Skins")
 
+local cr, cg, cb = DB.r, DB.g, DB.b
 local pairs = pairs
 local LE_QUEST_FREQUENCY_DAILY = LE_QUEST_FREQUENCY_DAILY or 2
 
@@ -51,4 +52,114 @@ function S:QuestTracker()
 		end
 	end
 	hooksecurefunc("QuestLog_Update", Showlevel)
+
+	local header = CreateFrame("Frame", nil, frame)
+	header:SetAllPoints(frame)
+	header:Hide()
+	B.CreateFS(header, 16, QUEST_LOG, true, "TOPLEFT", 40, 20)
+
+	local lineL = CreateFrame("Frame", nil, header)
+	B.CreateGF(lineL, 100, C.mult, "Horizontal", cr, cg, cb, 0, 1)
+	lineL:SetPoint("TOPRIGHT", header, "TOP")
+	local lineR = CreateFrame("Frame", nil, header)
+	B.CreateGF(lineR, 100, C.mult, "Horizontal", cr, cg, cb, 1, 0)
+	lineR:SetPoint("TOPLEFT", header, "TOP")
+
+	for i = 1, 30 do
+		local line = _G["QuestWatchLine"..i]
+		line:SetFont(DB.Font[1], DB.Font[2]+2, DB.Font[3])
+	end
+
+	local function OnMouseUp(self)
+		if IsShiftKeyDown() then -- untrack quest
+			local questID = GetQuestIDFromLogIndex(self.questIndex)
+			for index, value in ipairs(QUEST_WATCH_LIST) do
+				if value.id == questID then
+					tremove(QUEST_WATCH_LIST, index)
+				end
+			end
+			RemoveQuestWatch(self.questIndex)
+			QuestWatch_Update()
+		else -- open to quest log
+			if QuestLogEx then -- https://www.wowinterface.com/downloads/info24980-QuestLogEx.html
+				ShowUIPanel(QuestLogExFrame)
+				QuestLogEx:QuestLog_SetSelection(self.questIndex)
+				QuestLogEx:Maximize()
+			elseif ClassicQuestLog then -- https://www.wowinterface.com/downloads/info24937-ClassicQuestLogforClassic.html
+				ShowUIPanel(ClassicQuestLog)
+				QuestLog_SetSelection(self.questIndex)
+			else
+				ShowUIPanel(QuestLogFrame)
+				QuestLog_SetSelection(self.questIndex)
+				local valueStep = QuestLogListScrollFrame.ScrollBar:GetValueStep()
+				QuestLogListScrollFrame.ScrollBar:SetValue(self.questIndex*valueStep/2)
+			end
+		end
+		QuestLog_Update()
+	end
+
+	local function OnEnter(self)
+		if self.completed then
+			-- use normal colors instead as highlight
+			self.headerText:SetTextColor(.75, .61, 0)
+			for _, text in ipairs(self.objectiveTexts) do
+				text:SetTextColor(.8, .8, .8)
+			end
+		else
+			self.headerText:SetTextColor(NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b) -- 1, .82, 0
+			for _, text in ipairs(self.objectiveTexts) do
+				text:SetTextColor(HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b) -- 1, 1, 1
+			end
+		end
+	end
+
+	local ClickFrames = {}
+	local function SetClickFrame(watchIndex, questIndex, headerText, objectiveTexts, completed)
+		if not ClickFrames[watchIndex] then
+			ClickFrames[watchIndex] = CreateFrame("Frame")
+			ClickFrames[watchIndex]:SetScript("OnMouseUp", OnMouseUp)
+			ClickFrames[watchIndex]:SetScript("OnEnter", OnEnter)
+			ClickFrames[watchIndex]:SetScript("OnLeave", QuestWatch_Update)
+		end
+
+		local f = ClickFrames[watchIndex]
+		f:SetAllPoints(headerText)
+		f.watchIndex = watchIndex
+		f.questIndex = questIndex
+		f.headerText = headerText
+		f.objectiveTexts = objectiveTexts
+		f.completed = completed
+	end
+
+	hooksecurefunc("QuestWatch_Update", function()
+		header:SetShown(tracker:IsShown())
+
+		local watchTextIndex = 1
+		for i = 1, GetNumQuestWatches() do
+			local questIndex = GetQuestIndexForWatch(i)
+			if questIndex then
+				local numObjectives = GetNumQuestLeaderBoards(questIndex)
+				if numObjectives > 0 then
+					local headerText = _G["QuestWatchLine"..watchTextIndex]
+					watchTextIndex = watchTextIndex + 1
+					local objectivesGroup = {}
+					local objectivesCompleted = 0
+
+					for j = 1, numObjectives do
+						local finished = select(3, GetQuestLogLeaderBoard(j, questIndex))
+						if finished then
+							objectivesCompleted = objectivesCompleted + 1
+						end
+						tinsert(objectivesGroup, _G["QuestWatchLine"..watchTextIndex])
+						watchTextIndex = watchTextIndex + 1
+					end
+					SetClickFrame(i, questIndex, headerText, objectivesGroup, objectivesCompleted == numObjectives)
+				end
+			end
+		end
+		-- hide/show frames so it doesnt eat clicks, since we cant parent to a FontString
+		for _, frame in pairs(ClickFrames) do
+			frame[GetQuestIndexForWatch(frame.watchIndex) and "Show" or "Hide"](frame)
+		end
+	end)
 end
