bbb241d9a1980920f705aa1fb925c4f3f41b8857
siweia <siwei1990@gmail.com>
2019-09-06 15:03:25 +0800

    And the Classic Quest Log

--- Interface/AddOns/NDui/Modules/Skins/AddOns/QuestLogEx.lua
+++ Interface/AddOns/NDui/Modules/Skins/AddOns/QuestLogEx.lua
@@ -2,16 +2,14 @@ local _, ns = ...
 local B, C, L, DB, F, T = unpack(ns)
 local S = B:GetModule("Skins")
 
-function S:ReskinQuestGuru()
-	if not IsAddOnLoaded("QuestGuru") then return end
+function S:ReskinQuestTemplate(frame)
+	F.ReskinPortraitFrame(frame)
+	F.StripTextures(frame.count)
+	F.CreateBDFrame(frame.count, .25)
+	F.StripTextures(frame.scrollFrame.expandAll)
+	F.ReskinExpandOrCollapse(frame.scrollFrame.expandAll)
 
-	F.ReskinPortraitFrame(QuestGuru)
-	F.StripTextures(QuestGuru.count)
-	F.CreateBDFrame(QuestGuru.count, .25)
-	F.StripTextures(QuestGuruScrollFrame.expandAll)
-	F.ReskinExpandOrCollapse(QuestGuruScrollFrame.expandAll)
-
-	local mapButton = QuestGuru.mapButton
+	local mapButton = frame.mapButton
 	mapButton:SetSize(34, 22)
 	F.CreateBDFrame(mapButton)
 	mapButton:GetNormalTexture():SetTexCoord(.25, .73, .1, .4)
@@ -21,7 +19,7 @@ function S:ReskinQuestGuru()
 	hl:SetAllPoints()
 
 	hooksecurefunc("QuestLog_Update", function()
-		for _, bu in next, QuestGuru.scrollFrame.buttons do
+		for _, bu in next, frame.scrollFrame.buttons do
 			if bu:IsShown() and not bu.styled then
 				F.ReskinExpandOrCollapse(bu)
 				bu.styled = true
@@ -29,13 +27,40 @@ function S:ReskinQuestGuru()
 		end
 	end)
 
-	F.ReskinScroll(QuestGuruScrollFrameScrollBar)
-	F.ReskinScroll(QuestGuruDetailScrollFrameScrollBar)
-	F.Reskin(QuestGuru.abandon)
-	F.Reskin(QuestGuru.push)
-	F.Reskin(QuestGuru.track)
-	F.Reskin(QuestGuru.close)
+	F.ReskinScroll(frame.scrollFrame.scrollBar)
+	F.ReskinScroll(frame.detail.ScrollBar)
+	local names = {"abandon", "push", "track", "close", "options"}
+	for _, name in next, names do
+		local bu = frame[name]
+		if bu then F.Reskin(bu) end
+	end
+end
 
+function S:ReskinClassicQuestLog()
+	if not IsAddOnLoaded("Classic Quest Log") then return end
+
+	S:ReskinQuestTemplate(ClassicQuestLog)
+	ClassicQuestLog.scrollFrame.BG:SetAlpha(0)
+	ClassicQuestLog.detail.DetailBG:SetAlpha(0)
+
+	local optionsFrame = ClassicQuestLog.optionsFrame
+	F.StripTextures(optionsFrame)
+	F.ReskinClose(optionsFrame.CloseButton)
+	F.CreateSD(F.CreateBDFrame(optionsFrame))
+	optionsFrame:ClearAllPoints()
+	optionsFrame:SetPoint("TOPLEFT", ClassicQuestLog, "TOPRIGHT", 5, 0)
+
+	local names = {"UndockWindow", "LockWindow", "ShowResizeGrip", "ShowLevels", "ShowTooltips", "SolidBackground"}
+	for _, name in next, names do
+		local bu = optionsFrame[name]
+		if bu then F.ReskinCheck(bu) end
+	end
+end
+
+function S:ReskinQuestGuru()
+	if not IsAddOnLoaded("QuestGuru") then return end
+
+	S:ReskinQuestTemplate(QuestGuru)
 	-- Temp fix
 	if not QuestMapFrame_ShowQuestDetails then
 		QuestMapFrame_ShowQuestDetails = F.dummy
@@ -105,6 +130,7 @@ function S:ExtraQuestSkin()
 	if not NDuiDB["Skins"]["QuestLogEx"] then return end
 	if not F then return end
 
+	S:ReskinClassicQuestLog()
 	S:ReskinQuestGuru()
 	S:ReskinQuestLogEx()
 end
