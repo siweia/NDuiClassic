ff04d0b06ca23e80eb21a15053624c9194de007d
siweia <siwei1990@gmail.com>
2019-09-11 15:46:50 +0800

    Cleanup for the future

--- Interface/AddOns/NDui/Modules/Tooltip/SpecLevel.lua
+++ Interface/AddOns/NDui/Modules/Tooltip/SpecLevel.lua
@@ -8,7 +8,7 @@ local GetTime, CanInspect, NotifyInspect, ClearInspectPlayer, IsShiftKeyDown = G
 local UnitGUID, UnitClass, UnitIsUnit, UnitIsPlayer, UnitIsVisible, UnitIsDeadOrGhost, UnitOnTaxi = UnitGUID, UnitClass, UnitIsUnit, UnitIsPlayer, UnitIsVisible, UnitIsDeadOrGhost, UnitOnTaxi
 local GetInventoryItemTexture, GetInventoryItemLink, GetItemInfo, GetItemGem, GetAverageItemLevel = GetInventoryItemTexture, GetInventoryItemLink, GetItemInfo, GetItemGem, GetAverageItemLevel
 local GetSpecialization, GetSpecializationInfo, GetInspectSpecialization, GetSpecializationInfoByID = GetSpecialization, GetSpecializationInfo, GetInspectSpecialization, GetSpecializationInfoByID
-local HEIRLOOMS, LE_ITEM_QUALITY_ARTIFACT, LE_ITEM_QUALITY_HEIRLOOM = HEIRLOOMS, LE_ITEM_QUALITY_ARTIFACT, LE_ITEM_QUALITY_HEIRLOOM
+local HEIRLOOMS, LE_ITEM_QUALITY_HEIRLOOM = HEIRLOOMS, LE_ITEM_QUALITY_HEIRLOOM
 
 local specPrefix = SPECIALIZATION..": "..DB.InfoColor
 local levelPrefix = STAT_AVERAGE_ITEM_LEVEL..": "..DB.InfoColor
@@ -50,14 +50,12 @@ function TT:GetInspectInfo(...)
 	elseif self == "INSPECT_READY" then
 		local guid = ...
 		if guid == currentGUID then
-			local spec = TT:GetUnitSpec(currentUNIT)
 			local level = TT:GetUnitItemLevel(currentUNIT)
-			cache[guid].spec = spec
 			cache[guid].level = level
 			cache[guid].getTime = GetTime()
 
-			if spec and level then
-				TT:SetupSpecLevel(spec, level)
+			if level then
+				TT:SetupSpecLevel(level)
 			else
 				TT:InspectUnit(currentUNIT, true)
 			end
@@ -67,26 +65,17 @@ function TT:GetInspectInfo(...)
 end
 B:RegisterEvent("UNIT_INVENTORY_CHANGED", TT.GetInspectInfo)
 
-function TT:SetupSpecLevel(spec, level)
+function TT:SetupSpecLevel(_, level)
 	local _, unit = GameTooltip:GetUnit()
 	if not unit or UnitGUID(unit) ~= currentGUID then return end
 
-	local specLine, levelLine
+	local levelLine
 	for i = 2, GameTooltip:NumLines() do
 		local line = _G["GameTooltipTextLeft"..i]
 		local text = line:GetText()
-		if text and strfind(text, specPrefix) then
-			specLine = line
-		elseif text and strfind(text, levelPrefix) then
+		if text and strfind(text, levelPrefix) then
 			levelLine = line
 		end
-	end
-
-	spec = specPrefix..(spec or isPending)
-	if specLine then
-		specLine:SetText(spec)
-	else
-		GameTooltip:AddLine(spec)
 	end
 
 	level = levelPrefix..(level or isPending)
@@ -102,7 +91,7 @@ function TT:GetUnitItemLevel(unit)
 
 	local class = select(2, UnitClass(unit))
 	local ilvl, boa, total, haveWeapon, twohand = 0, 0, 0, 0, 0
-	local delay, mainhand, offhand, hasArtifact
+	local delay, mainhand, offhand
 	weapon[1], weapon[2] = 0, 0
 
 	for i = 1, 17 do
@@ -123,38 +112,23 @@ function TT:GetUnitItemLevel(unit)
 							boa = boa + 1
 						end
 
-						if unit ~= "player" then
-							level = B.GetItemLevel(itemLink) or level
-							if i < 16 then
-								total = total + level
-							elseif i > 15 and quality == LE_ITEM_QUALITY_ARTIFACT then
-								local relics = {select(4, strsplit(":", itemLink))}
-								for i = 1, 3 do
-									local relicID = relics[i] ~= "" and relics[i]
-									local relicLink = select(2, GetItemGem(itemLink, i))
-									if relicID and not relicLink then
-										delay = true
-										break
-									end
-								end
+						if i < 16 then
+							total = total + level
+						end
+
+						if i == 16 then
+							weapon[1] = level
+							haveWeapon = haveWeapon + 1
+							if slot == "INVTYPE_2HWEAPON" or slot == "INVTYPE_RANGED" or (slot == "INVTYPE_RANGEDRIGHT" and class == "HUNTER") then
+								mainhand = true
+								twohand = twohand + 1
 							end
-
-							if i == 16 then
-								if quality == LE_ITEM_QUALITY_ARTIFACT then hasArtifact = true end
-
-								weapon[1] = level
-								haveWeapon = haveWeapon + 1
-								if slot == "INVTYPE_2HWEAPON" or slot == "INVTYPE_RANGED" or (slot == "INVTYPE_RANGEDRIGHT" and class == "HUNTER") then
-									mainhand = true
-									twohand = twohand + 1
-								end
-							elseif i == 17 then
-								weapon[2] = level
-								haveWeapon = haveWeapon + 1
-								if slot == "INVTYPE_2HWEAPON" then
-									offhand = true
-									twohand = twohand + 1
-								end
+						elseif i == 17 then
+							weapon[2] = level
+							haveWeapon = haveWeapon + 1
+							if slot == "INVTYPE_2HWEAPON" then
+								offhand = true
+								twohand = twohand + 1
 							end
 						end
 					end
@@ -164,27 +138,23 @@ function TT:GetUnitItemLevel(unit)
 	end
 
 	if not delay then
-		if unit == "player" then
-			ilvl = select(2, GetAverageItemLevel())
-		else
-			if hasArtifact or twohand == 2 then
-				local higher = max(weapon[1], weapon[2])
-				total = total + higher*2
-			elseif twohand == 1 and haveWeapon == 1 then
-				total = total + weapon[1]*2 + weapon[2]*2
-			elseif twohand == 1 and haveWeapon == 2 then
-				if mainhand and weapon[1] >= weapon[2] then
-					total = total + weapon[1]*2
-				elseif offhand and weapon[2] >= weapon[1] then
-					total = total + weapon[2]*2
-				else
-					total = total + weapon[1] + weapon[2]
-				end
+		if twohand == 2 then
+			local higher = max(weapon[1], weapon[2])
+			total = total + higher*2
+		elseif twohand == 1 and haveWeapon == 1 then
+			total = total + weapon[1]*2 + weapon[2]*2
+		elseif twohand == 1 and haveWeapon == 2 then
+			if mainhand and weapon[1] >= weapon[2] then
+				total = total + weapon[1]*2
+			elseif offhand and weapon[2] >= weapon[1] then
+				total = total + weapon[2]*2
 			else
 				total = total + weapon[1] + weapon[2]
 			end
-			ilvl = total / 16
+		else
+			total = total + weapon[1] + weapon[2]
 		end
+		ilvl = total / 16
 
 		if ilvl > 0 then ilvl = format("%.1f", ilvl) end
 		if boa > 0 then ilvl = ilvl.." |cff00ccff("..boa..HEIRLOOMS..")" end
@@ -195,43 +165,22 @@ function TT:GetUnitItemLevel(unit)
 	return ilvl
 end
 
-function TT:GetUnitSpec(unit)
-	if not unit or UnitGUID(unit) ~= currentGUID then return end
-
-	local specName
-	if unit == "player" then
-		local specIndex = GetSpecialization()
-		if specIndex then
-			specName = select(2, GetSpecializationInfo(specIndex))
-		end
-	else
-		local specID = GetInspectSpecialization(unit)
-		if specID and specID > 0 then
-			specName = select(2, GetSpecializationInfoByID(specID))
-		end
-	end
-
-	return specName
-end
-
 function TT:InspectUnit(unit, forced)
-	local spec, level
+	local level
 
 	if UnitIsUnit(unit, "player") then
-		spec = self:GetUnitSpec("player")
 		level = self:GetUnitItemLevel("player")
-		self:SetupSpecLevel(spec, level)
+		self:SetupSpecLevel(nil, level)
 	else
 		if not unit or UnitGUID(unit) ~= currentGUID then return end
 		if not UnitIsPlayer(unit) then return end
 
 		local currentDB = cache[currentGUID]
-		spec = currentDB.spec
 		level = currentDB.level
-		self:SetupSpecLevel(spec, level)
+		self:SetupSpecLevel(nil, level)
 
 		if not NDuiDB["Tooltip"]["SpecLevelByShift"] and IsShiftKeyDown() then forced = true end
-		if spec and level and not forced and (GetTime() - currentDB.getTime < resetTime) then updater.elapsed = frequency return end
+		if level and not forced and (GetTime() - currentDB.getTime < resetTime) then updater.elapsed = frequency return end
 		if not UnitIsVisible(unit) or UnitIsDeadOrGhost("player") or UnitOnTaxi("player") then return end
 		if InspectFrame and InspectFrame:IsShown() then return end
 
