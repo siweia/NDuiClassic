c66e82bbed77ab20b1751284e2593975c8721b90
siweia <siwei1990@gmail.com>
2019-09-11 23:49:04 +0800

    Reminder back on track

--- Interface/AddOns/NDui/Core/Database.lua
+++ Interface/AddOns/NDui/Core/Database.lua
@@ -137,43 +137,20 @@ DB.BuffList = {
 -- Reminder Buffs Checklist
 DB.ReminderBuffs = {
 	MAGE = {
-		{	spells = {	-- 奥术魔宠
-				[210126] = true,
-			},
-			depend = 205022,
-			spec = 1,
-			combat = true,
-			instance = true,
-			pvp = true,
-		},
 		{	spells = {	-- 奥术智慧
 				[1459] = true,
 			},
 			depend = 1459,
+			combat = true,
 			instance = true,
+			pvp = true,
 		},
 	},
 	PRIEST = {
 		{	spells = {	-- 真言术耐
-				[21562] = true,
+				[1243] = true,
 			},
-			depend = 21562,
-			instance = true,
-		},
-	},
-	WARRIOR = {
-		{	spells = {	-- 战斗怒吼
-				[6673] = true,
-			},
-			depend = 6673,
-			instance = true,
-		},
-	},
-	SHAMAN = {
-		{	spells = {	-- 闪电之盾
-				[192106] = true,
-			},
-			depend = 192106,
+			depend = 1243,
 			combat = true,
 			instance = true,
 			pvp = true,
@@ -184,7 +161,6 @@ DB.ReminderBuffs = {
 				[2823] = true,		-- 致命药膏
 				[8679] = true,		-- 致伤药膏
 			},
-			spec = 1,
 			combat = true,
 			instance = true,
 			pvp = true,
@@ -192,7 +168,6 @@ DB.ReminderBuffs = {
 		{	spells = {	-- 效果类毒药
 				[3408] = true,		-- 减速药膏
 			},
-			spec = 1,
 			pvp = true,
 		},
 	},
--- Interface/AddOns/NDui/Modules/Auras/Reminder.lua
+++ Interface/AddOns/NDui/Modules/Auras/Reminder.lua
@@ -5,29 +5,27 @@ local A = B:GetModule("Auras")
 local groups = DB.ReminderBuffs[DB.MyClass]
 local iconSize = C.Auras.IconSize + 4
 local frames, parentFrame = {}
-local GetSpecialization, InCombatLockdown, GetZonePVPInfo, UnitInVehicle = GetSpecialization, InCombatLockdown, GetZonePVPInfo, UnitInVehicle
+local InCombatLockdown, GetZonePVPInfo = InCombatLockdown, GetZonePVPInfo
 local IsInInstance, IsPlayerSpell, UnitBuff, GetSpellTexture = IsInInstance, IsPlayerSpell, UnitBuff, GetSpellTexture
 local pairs, tinsert, next = pairs, table.insert, next
 
 function A:Reminder_Update(cfg)
 	local frame = cfg.frame
 	local depend = cfg.depend
-	local spec = cfg.spec
 	local combat = cfg.combat
 	local instance = cfg.instance
 	local pvp = cfg.pvp
-	local isPlayerSpell, isRightSpec, isInCombat, isInInst, isInPVP = true, true
+	local isPlayerSpell, isInCombat, isInInst, isInPVP = true
 	local inInst, instType = IsInInstance()
 
 	if depend and not IsPlayerSpell(depend) then isPlayerSpell = false end
-	if spec and spec ~= GetSpecialization() then isRightSpec = false end
 	if combat and InCombatLockdown() then isInCombat = true end
 	if instance and inInst and (instType == "scenario" or instType == "party" or instType == "raid") then isInInst = true end
 	if pvp and (instType == "arena" or instType == "pvp" or GetZonePVPInfo() == "combat") then isInPVP = true end
 	if not combat and not instance and not pvp then isInCombat, isInInst, isInPVP = true, true, true end
 
 	frame:Hide()
-	if isPlayerSpell and isRightSpec and (isInCombat or isInInst or isInPVP) and not UnitInVehicle("player") then
+	if isPlayerSpell and (isInCombat or isInInst or isInPVP) then
 		for i = 1, 32 do
 			local name, _, _, _, _, _, _, _, _, spellID = UnitBuff("player", i)
 			if not name then break end
@@ -79,7 +77,6 @@ end
 function A:InitReminder()
 	if not groups then return end
 	if not NDuiDB["Auras"]["Reminder"] then return end
-	if DB.isClassic then return end
 
 	parentFrame = CreateFrame("Frame", nil, UIParent)
 	parentFrame:SetPoint("CENTER", -220, 130)
