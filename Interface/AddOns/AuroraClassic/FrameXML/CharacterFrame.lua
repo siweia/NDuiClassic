ceac7eea07f5a9669edce61eb60c5d5c6f2949c5
siweia <siwei1990@gmail.com>
2019-09-12 22:17:22 +0800

    AuroraClassic: tidy up characterframe tabs #13

--- Interface/AddOns/AuroraClassic/FrameXML/CharacterFrame.lua
+++ Interface/AddOns/AuroraClassic/FrameXML/CharacterFrame.lua
@@ -4,6 +4,15 @@ tinsert(C.themes["AuroraClassic"], function()
 	F.ReskinPortraitFrame(CharacterFrame, 15, -15, -35, 73)
 
 	for i = 1, 4 do
-		F.ReskinTab(_G["CharacterFrameTab"..i])
+		local tab = _G["CharacterFrameTab"..i]
+		tab.bg = F.ReskinTab(tab)
+		if i == 1 then
+			tab:SetPoint("CENTER", CharacterFrame, "BOTTOMLEFT", 60, 59)
+		else
+			tab:SetPoint("LEFT", _G["CharacterFrameTab"..(i-1)], "RIGHT", -14, 0)
+		end
+		local hl = _G["CharacterFrameTab"..i.."HighlightTexture"]
+		hl:SetPoint("TOPLEFT", tab.bg, C.mult, -C.mult)
+		hl:SetPoint("BOTTOMRIGHT", tab.bg, -C.mult, C.mult)
 	end
 end)
