21d6fab41fe1a6a93c02ea60829fc679ee62472e
siweia <siwei1990@gmail.com>
2019-09-19 22:09:26 +0800

    Update reminder spell list

--- Interface/AddOns/NDui/Core/Database.lua
+++ Interface/AddOns/NDui/Core/Database.lua
@@ -139,7 +139,8 @@ DB.ReminderBuffs = {
 	MAGE = {
 		{	spells = {	-- 奥术智慧
 				[1459] = true,
-				[23028] = true,
+				[8096] = true,  -- 智力卷轴
+				[23028] = true, -- 奥术光辉
 			},
 			depend = 1459,
 			combat = true,
@@ -150,7 +151,8 @@ DB.ReminderBuffs = {
 	PRIEST = {
 		{	spells = {	-- 真言术耐
 				[1243] = true,
-				[21562] = true,
+				[8099] = true,  -- 耐力卷轴
+				[21562] = true, -- 坚韧祷言
 			},
 			depend = 1243,
 			combat = true,
