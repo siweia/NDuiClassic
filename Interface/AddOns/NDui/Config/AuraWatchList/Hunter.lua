5e1af54ab3fc915e8066aa9a41004e685a3d41a8
siweia <siwei1990@gmail.com>
2019-09-21 15:14:35 +0800

    Update auras for hunter

--- Interface/AddOns/NDui/Config/AuraWatchList/Hunter.lua
+++ Interface/AddOns/NDui/Config/AuraWatchList/Hunter.lua
@@ -17,7 +17,8 @@ local list = {
 		{AuraID = 24394, UnitID = "target", Caster = "pet"},		-- 胁迫
 	},
 	["Special Aura"] = {	-- 玩家重要光环组
-		{AuraID = 19574, UnitID = "player"},	-- 狂野怒火
+		{AuraID = 3045, UnitID = "player"},		-- 急速射击
+		{AuraID = 19574, UnitID = "pet"},		-- 狂野怒火
 	},
 	["Focus Aura"] = {		-- 焦点光环组
 	},
