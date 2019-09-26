fe9ac71bbf40ac49dc6c593b13b352d0187216eb
siweia <siwei1990@gmail.com>
2019-09-26 14:19:00 +0800

    Fix version check for idiots who using live version in classic

--- Interface/AddOns/NDui/Modules/Misc/Alerts.lua
+++ Interface/AddOns/NDui/Modules/Misc/Alerts.lua
@@ -102,8 +102,12 @@ end
 function M:VersionCheck_Compare(new, old)
 	local new1, new2 = strsplit(".", new)
 	new1, new2 = tonumber(new1), tonumber(new2)
+	if new1 >= 4 then new1, new2 = 0, 0 end
+
 	local old1, old2 = strsplit(".", old)
 	old1, old2 = tonumber(old1), tonumber(old2)
+	if old1 >= 4 then old1, old2 = 0, 0 end
+
 	if new1 > old1 or (new1 == old1 and new2 > old2) then
 		return "IsNew"
 	elseif new1 < old1 or (new1 == old1 and new2 < old2) then
@@ -119,6 +123,17 @@ function M:VersionCheck_Create(text)
 end
 
 local hasChecked
+function M:VersionCheck_Initial()
+	if not hasChecked then
+		if M:VersionCheck_Compare(NDuiADB["DetectVersion"], DB.Version) == "IsNew" then
+			local release = gsub(NDuiADB["DetectVersion"], "(%d)$", "0")
+			M:VersionCheck_Create(format(L["Outdated NDui"], release))
+		end
+
+		hasChecked = true
+	end
+end
+
 function M:VersionCheck_Update(...)
 	local prefix, msg, distType, author = ...
 	if prefix ~= "NDuiVersionCheck" then return end
@@ -131,14 +146,7 @@ function M:VersionCheck_Update(...)
 		C_ChatInfo_SendAddonMessage("NDuiVersionCheck", NDuiADB["DetectVersion"], distType)
 	end
 
-	if not hasChecked then
-		if M:VersionCheck_Compare(NDuiADB["DetectVersion"], DB.Version) == "IsNew" then
-			local release = gsub(NDuiADB["DetectVersion"], "(%d)$", "0")
-			M:VersionCheck_Create(format(L["Outdated NDui"], release))
-		end
-
-		hasChecked = true
-	end
+	M:VersionCheck_Initial()
 end
 
 local prevTime = 0
@@ -153,6 +161,7 @@ function M:VersionCheck()
 
 	B:RegisterEvent("CHAT_MSG_ADDON", self.VersionCheck_Update)
 
+	M:VersionCheck_Initial()
 	C_ChatInfo_RegisterAddonMessagePrefix("NDuiVersionCheck")
 	if IsInGuild() then
 		C_ChatInfo_SendAddonMessage("NDuiVersionCheck", DB.Version, "GUILD")
