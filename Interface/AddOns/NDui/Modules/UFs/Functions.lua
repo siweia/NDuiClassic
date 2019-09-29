b2caa06f69e9cfb044f1abe8293ab53ff56e72b8
siweia <siwei1990@gmail.com>
2019-09-29 12:13:32 +0800

    Heal prediction

--- Interface/AddOns/NDui/Modules/UFs/Functions.lua
+++ Interface/AddOns/NDui/Modules/UFs/Functions.lua
@@ -862,60 +862,25 @@ function UF:CreateExpRepBar(self)
 end
 
 function UF:CreatePrediction(self)
-	if DB.isClassic then return end
+	local myBar = CreateFrame("StatusBar", nil, self)
+	myBar:SetWidth(self:GetWidth())
+	myBar:SetPoint("TOP", self.Health, "TOP")
+	myBar:SetPoint("BOTTOM", self.Health, "BOTTOM")
+	myBar:SetPoint("LEFT", self.Health:GetStatusBarTexture(), "RIGHT")
+	myBar:SetStatusBarTexture(DB.normTex)
+	myBar:SetStatusBarColor(0, 1, .5, .5)
 
-	local mhpb = self:CreateTexture(nil, "BORDER", nil, 5)
-	mhpb:SetWidth(1)
-	mhpb:SetTexture(DB.normTex)
-	mhpb:SetVertexColor(0, 1, .5, .5)
+	local otherBar = CreateFrame("StatusBar", nil, self)
+	otherBar:SetWidth(self:GetWidth())
+	otherBar:SetPoint("TOP", self.Health, "TOP")
+	otherBar:SetPoint("BOTTOM", self.Health, "BOTTOM")
+	otherBar:SetPoint("LEFT", myBar:GetStatusBarTexture(), "RIGHT")
+	otherBar:SetStatusBarTexture(DB.normTex)
+	otherBar:SetStatusBarColor(0, 1, 0, .5)
 
-	local ohpb = self:CreateTexture(nil, "BORDER", nil, 5)
-	ohpb:SetWidth(1)
-	ohpb:SetTexture(DB.normTex)
-	ohpb:SetVertexColor(0, 1, 0, .5)
-
-	local abb = self:CreateTexture(nil, "BORDER", nil, 5)
-	abb:SetWidth(1)
-	abb:SetTexture(DB.normTex)
-	abb:SetVertexColor(.66, 1, 1, .7)
-
-	local abbo = self:CreateTexture(nil, "ARTWORK", nil, 1)
-	abbo:SetAllPoints(abb)
-	abbo:SetTexture("Interface\\RaidFrame\\Shield-Overlay", true, true)
-	abbo.tileSize = 32
-
-	local oag = self:CreateTexture(nil, "ARTWORK", nil, 1)
-	oag:SetWidth(15)
-	oag:SetTexture("Interface\\RaidFrame\\Shield-Overshield")
-	oag:SetBlendMode("ADD")
-	oag:SetAlpha(.7)
-	oag:SetPoint("TOPLEFT", self.Health, "TOPRIGHT", -5, 2)
-	oag:SetPoint("BOTTOMLEFT", self.Health, "BOTTOMRIGHT", -5, -2)
-
-	local hab = CreateFrame("StatusBar", nil, self)
-	hab:SetPoint("TOP")
-	hab:SetPoint("BOTTOM")
-	hab:SetPoint("RIGHT", self.Health:GetStatusBarTexture())
-	hab:SetWidth(self.Health:GetWidth())
-	hab:SetReverseFill(true)
-	hab:SetStatusBarTexture(DB.normTex)
-	hab:SetStatusBarColor(0, .5, .8, .5)
-
-	local ohg = self:CreateTexture(nil, "ARTWORK", nil, 1)
-	ohg:SetWidth(15)
-	ohg:SetTexture("Interface\\RaidFrame\\Absorb-Overabsorb")
-	ohg:SetBlendMode("ADD")
-	ohg:SetPoint("TOPRIGHT", self.Health, "TOPLEFT", 5, 2)
-	ohg:SetPoint("BOTTOMRIGHT", self.Health, "BOTTOMLEFT", 5, -2)
-
-	self.HealPredictionAndAbsorb = {
-		myBar = mhpb,
-		otherBar = ohpb,
-		absorbBar = abb,
-		absorbBarOverlay = abbo,
-		overAbsorbGlow = oag,
-		healAbsorbBar = hab,
-		overHealAbsorbGlow = ohg,
+	self.HealthPrediction = {
+		myBar = myBar,
+		otherBar = otherBar,
 		maxOverflow = 1,
 	}
 end
