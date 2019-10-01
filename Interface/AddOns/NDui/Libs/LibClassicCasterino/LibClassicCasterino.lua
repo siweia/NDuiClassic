704ec2ad75e69430ee5ac8a41f537c2a2be8650d
siweia <siwei1990@gmail.com>
2019-10-02 02:58:25 +0800

    Update LibClassicCasterino.lua

--- Interface/AddOns/NDui/Libs/LibClassicCasterino/LibClassicCasterino.lua
+++ Interface/AddOns/NDui/Libs/LibClassicCasterino/LibClassicCasterino.lua
@@ -4,7 +4,7 @@ Author: d87
 --]================]
 if WOW_PROJECT_ID ~= WOW_PROJECT_CLASSIC then return end
 
-local MAJOR, MINOR = "LibClassicCasterino", 17
+local MAJOR, MINOR = "LibClassicCasterino", 19
 local lib = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
 
@@ -25,6 +25,8 @@ local MOVECHECK_TIMEOUT = 4
 
 local UnitGUID = UnitGUID
 local bit_band = bit.band
+
+local GetSpellInfo = GetSpellInfo
 local GetTime = GetTime
 local CastingInfo = CastingInfo
 local ChannelInfo = ChannelInfo
@@ -40,9 +42,6 @@ local talentDecreased
 local crowdControlAuras
 local FireToUnits
 
-local AIMED_SHOT = GetSpellInfo(19434)
-local playerGUID = UnitGUID("player")
-
 f:SetScript("OnEvent", function(self, event, ...)
     return self[event](self, event, ...)
 end)
@@ -54,6 +53,10 @@ local NPCSpells
 
 local castTimeCache = {}
 local castTimeCacheStartTimes = setmetatable({}, { __mode = "v" })
+
+local AIMED_SHOT = GetSpellInfo(19434)
+local castingAimedShot = false
+local playerGUID = UnitGUID("player")
 
 --[[
 function DUMPCASTS()
@@ -115,10 +118,11 @@ local function CastStart(srcGUID, castType, spellName, spellID, overrideCastTime
 
     if castType == "CAST" then
         if srcGUID == playerGUID and spellName == AIMED_SHOT then
+            castingAimedShot = true
+            movecheckGUIDs[srcGUID] = MOVECHECK_TIMEOUT
             callbacks:Fire("UNIT_SPELLCAST_START", "player")
-        else
-            FireToUnits("UNIT_SPELLCAST_START", srcGUID)
         end
+        FireToUnits("UNIT_SPELLCAST_START", srcGUID)
     else
         FireToUnits("UNIT_SPELLCAST_CHANNEL_START", srcGUID)
     end
@@ -134,6 +138,10 @@ local function CastStop(srcGUID, castType, suffix )
 
         if castType == "CAST" then
             local event = "UNIT_SPELLCAST_"..suffix
+            if srcGUID == playerGUID and castingAimedShot then
+                castingAimedShot = false
+                callbacks:Fire(event, "player")
+            end
             FireToUnits(event, srcGUID)
         else
             FireToUnits("UNIT_SPELLCAST_CHANNEL_STOP", srcGUID)
@@ -256,8 +264,11 @@ local function IsSlowedDown(unit)
 end
 
 function lib:UnitCastingInfo(unit)
-    local name = CastingInfo()
-    if UnitIsUnit(unit,"player") and name then return CastingInfo() end
+    if UnitIsUnit(unit,"player") then
+        if not castingAimedShot then
+            return CastingInfo()
+        end
+    end
     local guid = UnitGUID(unit)
     local cast = casters[guid]
     if cast then
