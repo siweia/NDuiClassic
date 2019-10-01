34f0fdf7c75ec5ed4d2ae8f691c92db82e33c257
siweia <siwei1990@gmail.com>
2019-10-02 04:44:26 +0800

    Arrange GUI

--- Interface/AddOns/NDui/Core/GUI.lua
+++ Interface/AddOns/NDui/Core/GUI.lua
@@ -707,10 +707,10 @@ local optionList = { -- type, key, value, name, horizon, doubleline
 		{1, "Misc", "GemNEnchant", L["Show GemNEnchant"].."*", true},
 		{1, "Misc", "ShowItemLevel", L["Show ItemLevel"].."*"},
 		{},--blank
-		{1, "Misc", "Mail", L["Mail Tool"]},
-		{1, "ACCOUNT", "AutoBubbles", L["AutoBubbles"], true},
 		{1, "Misc", "FasterLoot", L["Faster Loot"].."*", nil, nil, updateFasterLoot},
 		{1, "Misc", "HideErrors", L["Hide Error"].."*", true, nil, updateErrorBlocker},
+		{1, "Misc", "Mail", L["Mail Tool"]},
+		{1, "ACCOUNT", "AutoBubbles", L["AutoBubbles"], true},
 		{1, "Misc", "EnhancedMenu", L["TargetEnhancedMenu"], nil, nil, nil, L["MenuEnhancedTips"]},
 		{1, "Misc", "AutoDismount", L["AutoDismount"], true},
 	},
