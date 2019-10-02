a8910716ccb5f23e73a3998cfcdae0f1befc649b
siweia <siwei1990@gmail.com>
2019-10-03 00:08:23 +0800

    New method to reset Details skin

--- Interface/AddOns/NDui/Core/GUI.lua
+++ Interface/AddOns/NDui/Core/GUI.lua
@@ -464,6 +464,20 @@ local function updateReminder()
 	B:GetModule("Auras"):InitReminder()
 end
 
+StaticPopupDialogs["RESET_DETAILS"] = {
+	text = L["Reset Details check"],
+	button1 = YES,
+	button2 = NO,
+	OnAccept = function()
+		NDuiADB["ResetDetails"] = true
+		ReloadUI()
+	end,
+	whileDead = 1,
+}
+local function resetDetails()
+	StaticPopup_Show("RESET_DETAILS")
+end
+
 -- Config
 local tabList = {
 	L["Actionbar"],
@@ -692,7 +706,7 @@ local optionList = { -- type, key, value, name, horizon, doubleline
 		{1, "Skins", "Bigwigs", L["Bigwigs Skin"]},
 		{1, "Skins", "TMW", L["TMW Skin"], true},
 		{1, "Skins", "WeakAuras", L["WeakAuras Skin"]},
-		{1, "Skins", "Details", L["Details Skin"], true},
+		{1, "Skins", "Details", L["Details Skin"], true, resetDetails},
 		{1, "Skins", "QuestLogEx", L["QuestLogEx Skin"], nil, nil, nil, L["ExtendedQuestLogAddons"]},
 		{1, "Skins", "Recount", L["Recount Skin"], true},
 	},
@@ -1330,15 +1344,6 @@ local function OpenGUI()
 		end
 	end
 	B:RegisterEvent("PLAYER_REGEN_DISABLED", showLater)
-
-	-- Reset Details skin
-	local detail = B.CreateButton(guiPage[10].child, 50, 25, RESET)
-	detail:SetPoint("TOPLEFT", 480, -300)
-	detail.text:SetTextColor(.6, .8, 1)
-	detail:SetScript("OnClick", function()
-		NDuiADB["ResetDetails"] = true
-		StaticPopup_Show("RELOAD_NDUI")
-	end)
 
 	SelectTab(1)
 end
