d01c2c8d20efcc2d521fe7630e0aa8c6b0b52db5
siweia <siwei1990@gmail.com>
2019-10-04 20:24:05 +0800

    Refactor shift key alert

--- Interface/AddOns/NDui/Modules/Bags/Core.lua
+++ Interface/AddOns/NDui/Modules/Bags/Core.lua
@@ -632,27 +632,4 @@ function module:OnLogin()
 		AuroraOptionsbags:Disable()
 		AuroraConfig.bags = false
 	end
-
-	-- SHIFT KEY DETECT
-	local function onUpdate(self, elapsed)
-		if IsShiftKeyDown() then
-			self.elapsed = self.elapsed + elapsed
-			if self.elapsed > 3 then
-				UIErrorsFrame:AddMessage(DB.InfoColor..L["StupidShiftKey"])
-				self:Hide()
-			end
-		end
-	end
-	local shiftUpdater = CreateFrame("Frame")
-	shiftUpdater:SetScript("OnUpdate", onUpdate)
-	shiftUpdater:Hide()
-
-	f.main:HookScript("OnShow", function()
-		shiftUpdater.elapsed = 0
-		shiftUpdater:Show()
-	end)
-
-	f.main:HookScript("OnHide", function()
-		shiftUpdater:Hide()
-	end)
 end
