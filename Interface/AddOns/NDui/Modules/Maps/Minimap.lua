1dcc55e9207494a1e1b3a8b4b94998941e376510
siweia <siwei1990@gmail.com>
2019-11-12 20:17:27 +0800

    Remove TrackingEye from recycle bin

--- Interface/AddOns/NDui/Modules/Maps/Minimap.lua
+++ Interface/AddOns/NDui/Modules/Maps/Minimap.lua
@@ -118,7 +118,7 @@ function module:RecycleBin()
 	local secureAddons = {
 		["HANDYNOTES"] = true,
 		["GUIDELIME"] = true,
-		--["TOWNSFOLKTRACKER"] = true,
+		["TRACKINGEYE"] = true,
 	}
 
 	local function isButtonSecure(name)
