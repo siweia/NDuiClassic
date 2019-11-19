e00db11cdbcf07bad9d6b839edfe0f9bff4c41cf
siweia <siwei1990@gmail.com>
2019-11-19 21:32:49 +0800

    AuroraClassic: reskin container and bank

--- Interface/AddOns/AuroraClassic/FrameXML/BankFrame.lua
+++ Interface/AddOns/AuroraClassic/FrameXML/BankFrame.lua
@@ -1,130 +1,50 @@
 local F, C = unpack(select(2, ...))
 
 tinsert(C.themes["AuroraClassic"], function()
-	if C.isClassic then return end
-
 	if not AuroraConfig.bags then return end
 
-	local r, g, b = C.r, C.g, C.b
-
-	-- [[ Bank ]]
-
+	BankFrame.CloseButton = BankCloseButton
+	F.ReskinPortraitFrame(BankFrame, 10, -10, -32, 70)
+	F.Reskin(BankFramePurchaseButton)
 	BankSlotsFrame:DisableDrawLayer("BORDER")
 	BankPortraitTexture:Hide()
-	BankFrameMoneyFrameInset:Hide()
-	BankFrameMoneyFrameBorder:Hide()
-
-	-- "item slots" and "bag slots" text
-	select(9, BankSlotsFrame:GetRegions()):SetDrawLayer("OVERLAY")
-	select(10, BankSlotsFrame:GetRegions()):SetDrawLayer("OVERLAY")
-
-	F.ReskinPortraitFrame(BankFrame)
-	F.Reskin(BankFramePurchaseButton)
-	F.ReskinTab(BankFrameTab1)
-	F.ReskinTab(BankFrameTab2)
-	F.ReskinInput(BankItemSearchBox)
-
-	local function onEnter(self)
-		self.bg:SetBackdropBorderColor(r, g, b)
-	end
-
-	local function onLeave(self)
-		self.bg:SetBackdropBorderColor(0, 0, 0)
-	end
 
 	local function styleBankButton(bu)
-		local border = bu.IconBorder
-		local questTexture = bu.IconQuestTexture
-		local searchOverlay = bu.searchOverlay
-
-		questTexture:SetDrawLayer("BACKGROUND")
-		questTexture:SetSize(1, 1)
-
-		border:SetTexture(C.media.backdrop)
-		border.SetTexture = F.dummy
-		border:SetPoint("TOPLEFT", -C.mult, C.mult)
-		border:SetPoint("BOTTOMRIGHT", C.mult, -C.mult)
-		border:SetDrawLayer("BACKGROUND", 1)
-
-		searchOverlay:SetPoint("TOPLEFT", -C.mult, C.mult)
-		searchOverlay:SetPoint("BOTTOMRIGHT", C.mult, -C.mult)
-
 		bu:SetNormalTexture("")
 		bu:SetPushedTexture("")
 		bu:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
 
 		bu.icon:SetTexCoord(.08, .92, .08, .92)
+		bu.bg = F.CreateBDFrame(bu, .25)
 
-		bu.bg = F.CreateBDFrame(bu, 0)
-
-		bu:HookScript("OnEnter", onEnter)
-		bu:HookScript("OnLeave", onLeave)
+		local searchOverlay = bu.searchOverlay
+		if searchOverlay then
+			searchOverlay:SetPoint("TOPLEFT", -C.mult, C.mult)
+			searchOverlay:SetPoint("BOTTOMRIGHT", C.mult, -C.mult)
+		end
 	end
 
-	for i = 1, 28 do
+	for i = 1, NUM_BANKGENERIC_SLOTS do
 		styleBankButton(_G["BankFrameItem"..i])
 	end
 
-	for i = 1, 7 do
-		local bag = BankSlotsFrame["Bag"..i]
-		local border = bag.IconBorder
-		local searchOverlay = bag.searchOverlay
-
-		bag:SetNormalTexture("")
-		bag:SetPushedTexture("")
-		bag:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
-		bag.SlotHighlightTexture:SetTexture(C.media.checked)
-
-		border:SetTexture(C.media.backdrop)
-		border.SetTexture = F.dummy
-		border:SetPoint("TOPLEFT", -C.mult, C.mult)
-		border:SetPoint("BOTTOMRIGHT", C.mult, -C.mult)
-		border:SetDrawLayer("BACKGROUND", 1)
-
-		searchOverlay:SetPoint("TOPLEFT", -C.mult, C.mult)
-		searchOverlay:SetPoint("BOTTOMRIGHT", C.mult, -C.mult)
-
-		bag.icon:SetTexCoord(.08, .92, .08, .92)
-
-		bag.bg = F.CreateBDFrame(bag, 0)
-
-		bag:HookScript("OnEnter", onEnter)
-		bag:HookScript("OnLeave", onLeave)
+	for i = 1, NUM_BANKBAGSLOTS do
+		styleBankButton(BankSlotsFrame["Bag"..i])
 	end
 
-	BankItemAutoSortButton:GetNormalTexture():SetTexCoord(.17, .83, .17, .83)
-	BankItemAutoSortButton:GetPushedTexture():SetTexCoord(.17, .83, .17, .83)
-	F.CreateBG(BankItemAutoSortButton)
-	local highlight = BankItemAutoSortButton:GetHighlightTexture()
-	highlight:SetColorTexture(1, 1, 1, .25)
-	highlight:SetAllPoints(BankItemAutoSortButton)
-
 	hooksecurefunc("BankFrameItemButton_Update", function(button)
-		if not button.isBag and button.IconQuestTexture:IsShown() then
-			button.IconBorder:SetVertexColor(1, 1, 0)
-		end
-	end)
+		if not button.bg then return end
 
-	-- [[ Reagent bank ]]
+		local container = button:GetParent():GetID()
+		local buttonID = button:GetID()
+		if button.isBag then container = -4 end
 
-	ReagentBankFrame:DisableDrawLayer("BACKGROUND")
-	ReagentBankFrame:DisableDrawLayer("BORDER")
-	ReagentBankFrame:DisableDrawLayer("ARTWORK")
-
-	F.Reskin(ReagentBankFrame.DespositButton)
-	F.Reskin(ReagentBankFrameUnlockInfoPurchaseButton)
-
-	-- make button more visible
-	F.StripTextures(ReagentBankFrameUnlockInfo)
-	ReagentBankFrameUnlockInfoBlackBG:SetColorTexture(.1, .1, .1)
-
-	local reagentButtonsStyled = false
-	ReagentBankFrame:HookScript("OnShow", function()
-		if not reagentButtonsStyled then
-			for i = 1, 98 do
-				styleBankButton(_G["ReagentBankFrameItem"..i])
-			end
-			reagentButtonsStyled = true
+		local texture, _, _, quality = GetContainerItemInfo(container, buttonID)
+		if texture and quality and quality > 1 then
+			local color = BAG_ITEM_QUALITY_COLORS[quality]
+			button.bg:SetBackdropBorderColor(color.r, color.g, color.b)
+		else
+			button.bg:SetBackdropBorderColor(0, 0, 0)
 		end
 	end)
 end)
--- Interface/AddOns/AuroraClassic/FrameXML/ContainerFrame.lua
+++ Interface/AddOns/AuroraClassic/FrameXML/ContainerFrame.lua
@@ -1,31 +1,13 @@
 local F, C = unpack(select(2, ...))
 
 tinsert(C.themes["AuroraClassic"], function()
-	if C.isClassic then return end
-
 	if not AuroraConfig.bags then return end
-
-	local r, g, b = C.r, C.g, C.b
-
-	BackpackTokenFrame:GetRegions():Hide()
-
-	local function onEnter(self)
-		self.bg:SetBackdropBorderColor(r, g, b)
-	end
-
-	local function onLeave(self)
-		self.bg:SetBackdropBorderColor(0, 0, 0)
-	end
 
 	for i = 1, 12 do
 		local con = _G["ContainerFrame"..i]
 		local name = _G["ContainerFrame"..i.."Name"]
 
-		for j = 1, 5 do
-			select(j, con:GetRegions()):SetAlpha(0)
-		end
-		select(7, con:GetRegions()):SetAlpha(0)
-
+		F.StripTextures(con, 0)
 		con.PortraitButton.Highlight:SetTexture("")
 
 		name:ClearAllPoints()
@@ -34,7 +16,6 @@ tinsert(C.themes["AuroraClassic"], function()
 		for k = 1, MAX_CONTAINER_ITEMS do
 			local item = "ContainerFrame"..i.."Item"..k
 			local button = _G[item]
-			local border = button.IconBorder
 			local searchOverlay = button.searchOverlay
 			local questTexture = _G[item.."IconQuestTexture"]
 			local newItemTexture = button.NewItemTexture
@@ -47,22 +28,14 @@ tinsert(C.themes["AuroraClassic"], function()
 			button:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
 
 			button.icon:SetTexCoord(.08, .92, .08, .92)
-
-			button.bg = F.CreateBDFrame(button, 0)
+			button.bg = F.CreateBDFrame(button, .25)
 
 			-- easiest way to 'hide' it without breaking stuff
 			newItemTexture:SetDrawLayer("BACKGROUND")
 			newItemTexture:SetSize(1, 1)
 
-			border:SetPoint("TOPLEFT", -C.mult, C.mult)
-			border:SetPoint("BOTTOMRIGHT", C.mult, -C.mult)
-			border:SetDrawLayer("BACKGROUND", 1)
-
 			searchOverlay:SetPoint("TOPLEFT", -C.mult, C.mult)
 			searchOverlay:SetPoint("BOTTOMRIGHT", C.mult, -C.mult)
-
-			button:HookScript("OnEnter", onEnter)
-			button:HookScript("OnLeave", onLeave)
 		end
 
 		local f = F.CreateBDFrame(con)
@@ -73,41 +46,21 @@ tinsert(C.themes["AuroraClassic"], function()
 		F.ReskinClose(_G["ContainerFrame"..i.."CloseButton"], "TOPRIGHT", con, "TOPRIGHT", -6, -6)
 	end
 
-	for i = 1, 3 do
-		local ic = _G["BackpackTokenFrameToken"..i.."Icon"]
-		ic:SetDrawLayer("OVERLAY")
-		ic:SetTexCoord(.08, .92, .08, .92)
-		F.CreateBG(ic)
-	end
-
-	F.ReskinInput(BagItemSearchBox)
-
 	hooksecurefunc("ContainerFrame_Update", function(frame)
 		local id = frame:GetID()
 		local name = frame:GetName()
 
-		if id == 0 then
-			BagItemSearchBox:ClearAllPoints()
-			BagItemSearchBox:SetPoint("TOPLEFT", frame, "TOPLEFT", 50, -35)
-			BagItemAutoSortButton:ClearAllPoints()
-			BagItemAutoSortButton:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -9, -31)
-		end
-
 		for i = 1, frame.size do
 			local itemButton = _G[name.."Item"..i]
-
-			itemButton.IconBorder:SetTexture(C.media.backdrop)
-			if _G[name.."Item"..i.."IconQuestTexture"]:IsShown() then
-				itemButton.IconBorder:SetVertexColor(1, 1, 0)
+			if itemButton.bg then
+				local texture, _, _, quality = GetContainerItemInfo(id, itemButton:GetID())
+				if texture and quality and quality > 1 then
+					local color = BAG_ITEM_QUALITY_COLORS[quality]
+					itemButton.bg:SetBackdropBorderColor(color.r, color.g, color.b)
+				else
+					itemButton.bg:SetBackdropBorderColor(0, 0, 0)
+				end
 			end
 		end
 	end)
-
-	BagItemAutoSortButton:GetNormalTexture():SetTexCoord(.17, .83, .17, .83)
-	BagItemAutoSortButton:GetPushedTexture():SetTexCoord(.17, .83, .17, .83)
-	F.CreateBG(BagItemAutoSortButton)
-
-	local highlight = BagItemAutoSortButton:GetHighlightTexture()
-	highlight:SetColorTexture(1, 1, 1, .25)
-	highlight:SetAllPoints(BagItemAutoSortButton)
 end)
