3e7381cadc087393dafd2f3c1c8f86aca1c6bf23
siweia <siwei1990@gmail.com>
2020-03-12 12:53:11 +0800

    Remove custom classcolor for Shaman #38

--- Interface/AddOns/NDui/Core/Database.lua
+++ Interface/AddOns/NDui/Core/Database.lua
@@ -16,11 +16,6 @@ for k, v in pairs(LOCALIZED_CLASS_NAMES_MALE) do
 	DB.ClassList[v] = k
 end
 DB.ClassColors = {}
--- Shaman classcolor
-RAID_CLASS_COLORS["SHAMAN"].r = 0
-RAID_CLASS_COLORS["SHAMAN"].g = .44
-RAID_CLASS_COLORS["SHAMAN"].b = .87
-RAID_CLASS_COLORS["SHAMAN"].colorStr = "ff0070dd"
 local colors = CUSTOM_CLASS_COLORS or RAID_CLASS_COLORS
 for class, value in pairs(colors) do
 	DB.ClassColors[class] = {}
