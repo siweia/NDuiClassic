f032ff93ecf9d4deee6b297da697ac8adf67cea5
siweia <siwei1990@gmail.com>
2020-03-16 14:33:25 +0800

    Old smooth out

--- Interface/AddOns/NDui/Core/Functions.lua
+++ Interface/AddOns/NDui/Core/Functions.lua
@@ -390,40 +390,6 @@ function B:HideOption()
 	self:SetScale(.0001)
 end
 
--- Smoothy
-local smoothing = {}
-local f = CreateFrame("Frame")
-f:SetScript("OnUpdate", function()
-	local limit = 30/GetFramerate()
-	for bar, value in pairs(smoothing) do
-		local cur = bar:GetValue()
-		local new = cur + min((value-cur)/8, max(value-cur, limit))
-		if new ~= new then
-			new = value
-		end
-		bar:SetValue_(new)
-		if cur == value or abs(new - value) < 1 then
-			smoothing[bar] = nil
-			bar:SetValue_(value)
-		end
-	end
-end)
-
-local function SetSmoothValue(self, value)
-	if value ~= self:GetValue() or value == 0 then
-		smoothing[self] = value
-	else
-		smoothing[self] = nil
-	end
-end
-
-function B:SmoothBar()
-	if not self.SetValue_ then
-		self.SetValue_ = self.SetValue
-		self.SetValue = SetSmoothValue
-	end
-end
-
 -- Timer Format
 local day, hour, minute = 86400, 3600, 60
 function B.FormatTime(s)
