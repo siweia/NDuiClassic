5212ae7a0ca4cd2504c516a93c3cd9a6897d4a0e
siweia <siwei1990@gmail.com>
2020-03-23 15:19:06 +0800

    Revert "Compatible with the LibTotemInfo"
    This reverts commit bcee49ca78a5d961bc4c2116735fbcab6094a813.

--- Interface/AddOns/NDui/Modules/Auras/AuraWatch.lua
+++ Interface/AddOns/NDui/Modules/Auras/AuraWatch.lua
@@ -405,7 +405,8 @@ function A:AuraWatch_UpdateCD()
 						end
 					end
 				elseif value.TotemID then
-					local haveTotem, name, start, duration, icon, id = GetTotemInfo(value.TotemID)
+					local haveTotem, name, start, duration, icon = GetTotemInfo(value.TotemID)
+					local id = select(7, GetSpellInfo(name))
 					if haveTotem then
 						if group.Mode:lower() == "icon" then name = nil end
 						A:AuraWatch_SetupCD(KEY, name, icon, start, duration, false, 1, id)
