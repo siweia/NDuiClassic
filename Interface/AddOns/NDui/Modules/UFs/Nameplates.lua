76493530049a2567a7d859fb6c26f9b89769a68d
siweia <siwei1990@outlook.com>
2020-06-25 10:24:01 +0800

    Fix health color update

--- Interface/AddOns/NDui/Modules/UFs/Nameplates.lua
+++ Interface/AddOns/NDui/Modules/UFs/Nameplates.lua
@@ -86,8 +86,10 @@ function UF:UpdateUnitPower()
 end
 
 -- Update unit color
-function UF.UpdateColor(element, unit)
-	local self = element.__owner
+function UF.UpdateColor(self, _, unit)
+	if not unit or self.unit ~= unit then return end
+
+	local element = self.Health
 	local name = self.unitName
 	local npcID = self.npcID
 	local isCustomUnit = customUnits[name] or customUnits[npcID]
@@ -136,7 +138,7 @@ end
 function UF:UpdateThreatColor(_, unit)
 	if unit ~= self.unit then return end
 
-	UF.UpdateColor(self.Health, unit)
+	UF.UpdateColor(self, _, unit)
 end
 
 function UF:CreateThreatColor(self)
