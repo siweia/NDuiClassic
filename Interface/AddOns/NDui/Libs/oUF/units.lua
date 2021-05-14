6b0ff407278c6ea7ad030ebe666b21d9a33f775a
siweia <siwei1990@outlook.com>
2021-05-14 18:49:19 +0800

    Missing unit handle for focus

--- Interface/AddOns/NDui/Libs/oUF/units.lua
+++ Interface/AddOns/NDui/Libs/oUF/units.lua
@@ -11,6 +11,8 @@ function oUF:HandleUnit(object, unit)
 		object:RegisterEvent('PLAYER_TARGET_CHANGED', object.UpdateAllElements, true)
 	elseif(unit == 'mouseover') then
 		object:RegisterEvent('UPDATE_MOUSEOVER_UNIT', object.UpdateAllElements, true)
+	elseif(unit == 'focus') then
+		object:RegisterEvent('PLAYER_FOCUS_CHANGED', object.UpdateAllElements, true)
 	elseif(unit and unit:match('%w+target')) then
 		enableTargetUpdate(object)
 	end
