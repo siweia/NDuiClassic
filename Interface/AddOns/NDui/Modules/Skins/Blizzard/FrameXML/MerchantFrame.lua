af32753b6648ed8c8006e11cd9a7020adc3b1f6f
siweia <siwei1990@outlook.com>
2021-06-04 00:36:49 +0800

    Skins: merchant item quality

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/MerchantFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/MerchantFrame.lua
@@ -1,6 +1,52 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
+local function reskinMerchantItem(item)
+	local name = item.Name
+	local button = item.ItemButton
+	local icon = button.icon
+	local moneyFrame = _G[item:GetName().."MoneyFrame"]
+
+	B.StripTextures(item)
+	B.CreateBDFrame(item, .25)
+
+	B.StripTextures(button)
+	button:ClearAllPoints()
+	button:SetPoint("LEFT", item, 4, 0)
+	local hl = button:GetHighlightTexture()
+	hl:SetColorTexture(1, 1, 1, .25)
+	hl:SetInside()
+
+	icon:SetInside()
+	button.bg = B.ReskinIcon(icon)
+	button.IconBorder:SetAlpha(0)
+
+	name:SetFontObject(Game12Font)
+	name:SetPoint("LEFT", button, "RIGHT", 2, 9)
+	moneyFrame:SetPoint("BOTTOMLEFT", button, "BOTTOMRIGHT", 3, 0)
+end
+
+local function reskinMerchantInteract(button)
+	button:SetPushedTexture("")
+	button:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
+	B.CreateBDFrame(button)
+end
+
+local function UpdateMerchantItemQuality(self, link)
+	local quality = link and select(3, GetItemInfo(link))
+	local textR, textG, textB = 1, 1, 1
+	local borderR, borderG, borderB = 0, 0, 0
+	if quality then
+		textR, textG, textB = GetItemQualityColor(quality)
+		local color = DB.QualityColors[quality]
+		borderR, borderG, borderB = color.r, color.g, color.b
+	else
+		MerchantFrame_RegisterForQualityUpdates()
+	end
+	self.Name:SetTextColor(textR, textG, textB)
+	self.ItemButton.bg:SetBackdropBorderColor(borderR, borderG, borderB)
+end
+
 tinsert(C.defaultThemes, function()
 	B.ReskinPortraitFrame(MerchantFrame)
 --	B.ReskinDropDown(MerchantFrameLootFilter)
@@ -25,28 +71,8 @@ tinsert(C.defaultThemes, function()
 
 	for i = 1, BUYBACK_ITEMS_PER_PAGE do
 		local item = _G["MerchantItem"..i]
-		local name = item.Name
-		local button = item.ItemButton
-		local icon = button.icon
-		local moneyFrame = _G["MerchantItem"..i.."MoneyFrame"]
+		reskinMerchantItem(item)
 
-		B.StripTextures(item)
-		B.CreateBDFrame(item, .25)
-
-		B.StripTextures(button)
-		button.IconBorder:SetAlpha(0)
-		button:ClearAllPoints()
-		button:SetPoint("LEFT", item, 4, 0)
-		local hl = button:GetHighlightTexture()
-		hl:SetColorTexture(1, 1, 1, .25)
-		hl:SetInside()
-
-		B.ReskinIcon(icon)
-		icon:SetInside()
-		name:SetFontObject(Game12Font)
-		name:SetPoint("LEFT", button, "RIGHT", 2, 9)
-
-		moneyFrame:SetPoint("BOTTOMLEFT", button, "BOTTOMRIGHT", 3, 0)
 		for j = 1, 3 do
 			local currency = _G["MerchantItem"..i.."AltCurrencyFrameItem"..j]
 			local texture = _G["MerchantItem"..i.."AltCurrencyFrameItem"..j.."Texture"]
@@ -55,82 +81,10 @@ tinsert(C.defaultThemes, function()
 		end
 	end
 
-	hooksecurefunc("MerchantFrame_UpdateMerchantInfo", function()
-		local numMerchantItems = GetMerchantNumItems()
-		for i = 1, MERCHANT_ITEMS_PER_PAGE do
-			local index = ((MerchantFrame.page - 1) * MERCHANT_ITEMS_PER_PAGE) + i
-			if index <= numMerchantItems then
-				local _, _, price, _, _, _, extendedCost = GetMerchantItemInfo(index)
-				if extendedCost and (price <= 0) then
-					_G["MerchantItem"..i.."AltCurrencyFrame"]:SetPoint("BOTTOMLEFT", "MerchantItem"..i.."NameFrame", "BOTTOMLEFT", 0, 35)
-				end
+	hooksecurefunc("MerchantFrameItem_UpdateQuality", UpdateMerchantItemQuality)
 
-				local bu = _G["MerchantItem"..i.."ItemButton"]
-				local name = _G["MerchantItem"..i.."Name"]
-				if bu.link then
-					local _, _, quality = GetItemInfo(bu.link)
-					if quality then
-						local r, g, b = GetItemQualityColor(quality)
-						name:SetTextColor(r, g, b)
-					else
-						name:SetTextColor(1, 1, 1)
-					end
-				else
-					name:SetTextColor(1, 1, 1)
-				end
-			end
-		end
-
-		local name = GetBuybackItemLink(GetNumBuybackItems())
-		if name then
-			local _, _, quality = GetItemInfo(name)
-			local r, g, b = GetItemQualityColor(quality or 1)
-
-			MerchantBuyBackItemName:SetTextColor(r, g, b)
-		end
-	end)
-
-	hooksecurefunc("MerchantFrame_UpdateBuybackInfo", function()
-		for i = 1, BUYBACK_ITEMS_PER_PAGE do
-			local itemLink = GetBuybackItemLink(i)
-			local name = _G["MerchantItem"..i.."Name"]
-
-			if itemLink then
-				local _, _, quality = GetItemInfo(itemLink)
-				local r, g, b = GetItemQualityColor(quality)
-
-				name:SetTextColor(r, g, b)
-			else
-				name:SetTextColor(1, 1, 1)
-			end
-		end
-	end)
-
-	MerchantBuyBackItemSlotTexture:SetAlpha(0)
-	MerchantBuyBackItemNameFrame:Hide()
-	MerchantBuyBackItemItemButton:SetNormalTexture("")
-	MerchantBuyBackItemItemButton:SetPushedTexture("")
-	MerchantBuyBackItemItemButton:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
-	MerchantBuyBackItemItemButton:GetHighlightTexture():SetAllPoints(MerchantBuyBackItemItemButtonIconTexture)
-	MerchantBuyBackItemItemButton.IconBorder:SetAlpha(0)
-
-	B.CreateBDFrame(MerchantBuyBackItemItemButton, 0)
-	B.CreateBDFrame(MerchantBuyBackItem, .25)
-
-	MerchantBuyBackItemItemButtonIconTexture:SetTexCoord(.08, .92, .08, .92)
-	MerchantBuyBackItemItemButtonIconTexture:ClearAllPoints()
-	MerchantBuyBackItemItemButtonIconTexture:SetPoint("TOPLEFT", C.mult, -C.mult)
-	MerchantBuyBackItemItemButtonIconTexture:SetPoint("BOTTOMRIGHT", -C.mult, C.mult)
-
-	MerchantBuyBackItemName:SetHeight(25)
-	MerchantBuyBackItemName:ClearAllPoints()
-	MerchantBuyBackItemName:SetPoint("LEFT", MerchantBuyBackItemSlotTexture, "RIGHT", -5, 8)
-
-	local function reskinMerchantInteract(button)
-		button:SetPushedTexture("")
-		button:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
-		B.CreateBDFrame(button)
-	end
+	MerchantBuyBackItem:SetHeight(44)
+	reskinMerchantItem(MerchantBuyBackItem)
 
 	reskinMerchantInteract(MerchantGuildBankRepairButton)
 	MerchantGuildBankRepairButtonIcon:SetTexCoord(.595, .8075, .05, .52)
@@ -156,4 +110,14 @@ tinsert(C.defaultThemes, function()
 			end
 		end
 	end)
+
+	-- StackSplitFrame
+
+	local StackSplitFrame = StackSplitFrame
+	B.StripTextures(StackSplitFrame)
+	B.SetBD(StackSplitFrame, nil, 0, -10, 0, 10)
+	B.Reskin(StackSplitOkayButton)
+	B.Reskin(StackSplitCancelButton)
+	B.ReskinArrow(StackSplitLeftButton, "left")
+	B.ReskinArrow(StackSplitRightButton, "right")
 end)
