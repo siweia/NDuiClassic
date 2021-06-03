e0466d842b8f483660ecbf2f7c4c271ea6555d33
siweia <siwei1990@outlook.com>
2021-06-04 01:27:43 +0800

    Skins: merge bank and container

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/Bags.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/Bags.lua
@@ -28,9 +28,28 @@ local function createBagIcon(frame, index)
 	end
 end
 
+local function styleBankButton(bu)
+	bu:SetNormalTexture("")
+	bu:SetPushedTexture("")
+	bu:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
+	bu.searchOverlay:SetOutside()
+
+	bu.icon:SetTexCoord(unpack(DB.TexCoord))
+	bu.bg = B.CreateBDFrame(bu.icon, .25)
+	B.ReskinIconBorder(bu.IconBorder)
+
+	local questTexture = bu.IconQuestTexture
+	if questTexture then
+		questTexture:SetDrawLayer("BACKGROUND")
+		questTexture:SetSize(1, 1)
+	end
+end
+
 tinsert(C.defaultThemes, function()
 	if C.db["Bags"]["Enable"] then return end
 	if not C.db["Skins"]["DefaultBags"] then return end
+
+	-- Bags
 
 	for i = 1, 12 do
 		local con = _G["ContainerFrame"..i]
@@ -85,6 +104,28 @@ tinsert(C.defaultThemes, function()
 				local icon = GetInventoryItemTexture("player", invID)
 				frame.bagIcon:SetTexture(icon or backpackTexture)
 			end
+		end
+	end)
+
+	-- Bank
+
+	BankFrame.CloseButton = BankCloseButton
+	B.ReskinPortraitFrame(BankFrame, 25, -10, 0, 70)
+	B.Reskin(BankFramePurchaseButton)
+	BankSlotsFrame:DisableDrawLayer("BORDER")
+	BankPortraitTexture:Hide()
+
+	for i = 1, NUM_BANKGENERIC_SLOTS do
+		styleBankButton(_G["BankFrameItem"..i])
+	end
+
+	for i = 1, NUM_BANKBAGSLOTS do
+		styleBankButton(BankSlotsFrame["Bag"..i])
+	end
+
+	hooksecurefunc("BankFrameItemButton_Update", function(button)
+		if not button.isBag and button.IconQuestTexture:IsShown() then
+			button.IconBorder:SetVertexColor(1, 1, 0)
 		end
 	end)
 end)
