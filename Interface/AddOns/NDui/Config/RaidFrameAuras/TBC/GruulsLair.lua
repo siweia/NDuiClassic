a817dfb98203ad4324618dd5c1dd4b0a605397bb
siweia <siwei1990@outlook.com>
2021-06-26 22:39:21 +0800

    Update raid debuffs

--- Interface/AddOns/NDui/Config/RaidFrameAuras/TBC/GruulsLair.lua
+++ Interface/AddOns/NDui/Config/RaidFrameAuras/TBC/GruulsLair.lua
@@ -5,4 +5,16 @@ local module = B:GetModule("AurasTable")
 local TIER = 2
 local INSTANCE = 565 -- 格鲁尔的巢穴
 
-module:RegisterDebuff(TIER, INSTANCE, 0, 24099) -- 荒芜诅咒
+-- 莫加尔
+module:RegisterDebuff(TIER, INSTANCE, 0, 36032) -- 奥术冲击
+module:RegisterDebuff(TIER, INSTANCE, 0, 11726) -- 奴役恶魔
+module:RegisterDebuff(TIER, INSTANCE, 0, 33129) -- 黑暗凋零
+module:RegisterDebuff(TIER, INSTANCE, 0, 33175) -- 奥术震击
+module:RegisterDebuff(TIER, INSTANCE, 0, 33061) -- 冲击波
+module:RegisterDebuff(TIER, INSTANCE, 0, 33130) -- 死亡缠绕
+module:RegisterDebuff(TIER, INSTANCE, 0, 16508) -- 破胆咆哮
+-- 格鲁尔
+module:RegisterDebuff(TIER, INSTANCE, 0, 38927) -- 魔能疼痛
+module:RegisterDebuff(TIER, INSTANCE, 0, 36240) -- 洞穴震颤
+module:RegisterDebuff(TIER, INSTANCE, 0, 33652) -- 石化
+module:RegisterDebuff(TIER, INSTANCE, 0, 33525) -- 大地冲击
--- Interface/AddOns/NDui/Config/RaidFrameAuras/TBC/MagtheridonsLair.lua
+++ Interface/AddOns/NDui/Config/RaidFrameAuras/TBC/MagtheridonsLair.lua
@@ -5,4 +5,7 @@ local module = B:GetModule("AurasTable")
 local TIER = 2
 local INSTANCE = 544 -- 玛瑟里顿的巢穴
 
-module:RegisterDebuff(TIER, INSTANCE, 0, 24099) -- 荒芜诅咒
+-- 玛瑟里顿
+module:RegisterDebuff(TIER, INSTANCE, 0, 44032) -- 心灵疲倦
+module:RegisterDebuff(TIER, INSTANCE, 0, 30530) -- 恐惧
+module:RegisterDebuff(TIER, INSTANCE, 0, 38927) -- 魔能疼痛
