77b9822ac60eab8f3aae6493094f5f274134ce1c
siweia <siwei1990@outlook.com>
2021-07-11 16:42:57 +0800

    Update minimap recycle anchoring

--- Interface/AddOns/NDui/Modules/Maps/Minimap.lua
+++ Interface/AddOns/NDui/Modules/Maps/Minimap.lua
@@ -132,7 +132,7 @@ function module:RecycleBin()
 
 	local width, height, alpha = 220, 40, .5
 	local bin = CreateFrame("Frame", "RecycleBinFrame", UIParent)
-	bin:SetPoint("RIGHT", bu, "LEFT", -3, -6)
+	bin:SetPoint("BOTTOMRIGHT", bu, "BOTTOMLEFT", -3, 10)
 	bin:SetSize(width, height)
 	bin:SetFrameStrata("TOOLTIP")
 	bin:Hide()
@@ -267,25 +267,23 @@ function module:RecycleBin()
 			end
 		end
 
-		local lastbutton
-		for index, button in pairs(shownButtons) do
-			button:ClearAllPoints()
-			if not lastbutton then
-				button:SetPoint("BOTTOMRIGHT", bin, -3, 3)
-			elseif mod(index, iconsPerRow) == 1 then
-				button:SetPoint("BOTTOM", shownButtons[index - iconsPerRow], "TOP", 0, 3)
-			else
-				button:SetPoint("RIGHT", lastbutton, "LEFT", -3, 0)
-			end
-			lastbutton = button
-		end
-
 		local numShown = #shownButtons
 		local row = numShown == 0 and 1 or B:Round((numShown + rowMult) / iconsPerRow)
 		local newHeight = row*37 + 3
 		bin:SetHeight(newHeight)
 		tex:SetHeight(newHeight)
 		rightLine:SetHeight(newHeight + 2*C.mult)
+
+		for index, button in pairs(shownButtons) do
+			button:ClearAllPoints()
+			if index == 1 then
+				button:SetPoint("BOTTOMRIGHT", bin, -3, 3)
+			elseif row > 1 and mod(index, row) == 1 or row == 1 then
+				button:SetPoint("RIGHT", shownButtons[index - row], "LEFT", -3, 0)
+			else
+				button:SetPoint("BOTTOM", shownButtons[index - 1], "TOP", 0, 3)
+			end
+		end
 	end
 
 	bu:SetScript("OnClick", function()
