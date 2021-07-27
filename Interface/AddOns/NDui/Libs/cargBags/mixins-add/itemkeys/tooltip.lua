9859360d20ba793811b72d5c236fa5699780e7d1
siweia <siwei1990@outlook.com>
2021-07-27 13:19:42 +0800

    Tooltip range tweaks

--- Interface/AddOns/NDui/Libs/cargBags/mixins-add/itemkeys/tooltip.lua
+++ Interface/AddOns/NDui/Libs/cargBags/mixins-add/itemkeys/tooltip.lua
@@ -45,7 +45,7 @@ cargBags.itemKeys["bindOn"] = function(i)
 	tip:SetOwner(UIParent, "ANCHOR_NONE")
 	tip:SetBagItem(i.bagID, i.slotID)
  
-	for j = 2, 4 do
+	for j = 2, 5 do
 		local line = _G["NDui_ScanTooltipTextLeft"..j]
 		local lineText = line and line:GetText()
 		if not lineText then break end
