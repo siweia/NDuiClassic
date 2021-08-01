f4532d1ca6da8b0815ac91209f264bbfc26f16e2
siweia <siwei1990@outlook.com>
2021-08-01 19:36:11 +0800

    Update raid debuffs in phase 2, L9429

--- Interface/AddOns/NDui/Config/RaidFrameAuras/TBC/TempestKeep.lua
+++ Interface/AddOns/NDui/Config/RaidFrameAuras/TBC/TempestKeep.lua
@@ -3,6 +3,23 @@ local B, C, L, DB = unpack(ns)
 local module = B:GetModule("AurasTable")
 
 local TIER = 2
-local INSTANCE = 550 -- 风暴神殿
+local INSTANCE = 550 -- 风暴要塞
 
-module:RegisterDebuff(TIER, INSTANCE, 0, 24099) -- 荒芜诅咒
+-- 奥
+module:RegisterDebuff(TIER, INSTANCE, 0, 35383) -- 烈焰之地
+module:RegisterDebuff(TIER, INSTANCE, 0, 35410) -- 融化护甲
+-- 魔能机甲
+module:RegisterDebuff(TIER, INSTANCE, 0, 34190) -- 奥术宝珠
+-- 大星术师
+module:RegisterDebuff(TIER, INSTANCE, 0, 33023) -- 索兰莉安印记
+module:RegisterDebuff(TIER, INSTANCE, 0, 33044) -- 星术师之怒
+module:RegisterDebuff(TIER, INSTANCE, 0, 33045) -- 星术师之怒
+-- 凯尔萨斯·逐日者
+module:RegisterDebuff(TIER, INSTANCE, 0, 36970) -- 奥术爆裂（星术师卡波妮娅）
+module:RegisterDebuff(TIER, INSTANCE, 0, 37018) -- 燃烧（星术师卡波妮娅）
+module:RegisterDebuff(TIER, INSTANCE, 0, 44863) -- 咆哮（萨古纳尔男爵）
+module:RegisterDebuff(TIER, INSTANCE, 0, 37027) -- 遥控玩具（首席技师塔隆尼库斯）
+module:RegisterDebuff(TIER, INSTANCE, 0, 36965) -- 撕裂（亵渎者萨拉德雷）
+module:RegisterDebuff(TIER, INSTANCE, 0, 30225) -- 沉默（亵渎者萨拉德雷）
+module:RegisterDebuff(TIER, INSTANCE, 0, 36834) -- 奥术干扰（凯尔萨斯·逐日者）
+module:RegisterDebuff(TIER, INSTANCE, 0, 36797) -- 精神控制（凯尔萨斯·逐日者）
