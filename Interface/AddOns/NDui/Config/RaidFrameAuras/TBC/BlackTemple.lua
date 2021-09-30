24b439b08e16196fbef3a9057f2005c39ab1afd2
siweia <siwei1990@outlook.com>
2021-09-30 23:13:34 +0800

    Remove TBC auras

--- Interface/AddOns/NDui/Config/RaidFrameAuras/TBC/BlackTemple.lua
+++
@@ -1,8 +0,0 @@
-local _, ns = ...
-local B, C, L, DB = unpack(ns)
-local module = B:GetModule("AurasTable")
-
-local TIER = 2
-local INSTANCE = 564 -- 黑暗神殿
-
-module:RegisterDebuff(TIER, INSTANCE, 0, 24099) -- 荒芜诅咒
--- Interface/AddOns/NDui/Config/RaidFrameAuras/TBC/SunwellPlateau.lua
+++
@@ -1,8 +0,0 @@
-local _, ns = ...
-local B, C, L, DB = unpack(ns)
-local module = B:GetModule("AurasTable")
-
-local TIER = 2
-local INSTANCE = 580 -- 太阳之井
-
-module:RegisterDebuff(TIER, INSTANCE, 0, 24099) -- 荒芜诅咒
