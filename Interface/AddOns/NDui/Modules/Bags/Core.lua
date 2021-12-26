25c024f51415a9a16506577e9a636be19aadb8a8
siweia <siwei1990@outlook.com>
2021-12-26 15:33:12 +0800

    Updates from 6.18.6

--- Interface/AddOns/NDui/Modules/Bags/Core.lua
+++ Interface/AddOns/NDui/Modules/Bags/Core.lua
@@ -78,9 +78,9 @@ local function highlightFunction(button, match)
 	button.searchOverlay:SetShown(not match)
 end
 
-local function IsItemMatched(type, text)
-	if not type or type == "" then return end
-	return strmatch(type, text)
+local function IsItemMatched(str, text)
+	if not str or str == "" then return end
+	return strmatch(strlower(str), text)
 end
 
 local BagSmartFilter = {
