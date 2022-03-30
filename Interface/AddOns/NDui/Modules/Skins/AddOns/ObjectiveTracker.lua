553b392a7613bbd858db5f5973d6c51447c29163
siweia <siwei1990@outlook.com>
2022-03-30 15:04:46 +0800

    Remove EnhancedQuestLog

--- Interface/AddOns/NDui/Modules/Skins/AddOns/ObjectiveTracker.lua
+++ Interface/AddOns/NDui/Modules/Skins/AddOns/ObjectiveTracker.lua
@@ -16,112 +16,6 @@ local headerString = QUESTS_LABEL.." %s/%s"
 
 local frame
 
-function S:EnhancedQuestLog()
-	if QuestLogFrame:GetWidth() > 700 then return end
-
-	-- LeatrixPlus EnhancedQuestLog
-	-- Make the quest log frame double-wide
-	UIPanelWindows["QuestLogFrame"] = { area = "override", pushable = 0, xoffset = -16, yoffset = 12, bottomClampOverride = 140 + 12, width = 714, height = 487, whileDead = 1}
-
-	-- Size the quest log frame
-	QuestLogFrame:SetWidth(714)
-	QuestLogFrame:SetHeight(487)
-
-	-- Adjust quest log title text
-	QuestLogTitleText:ClearAllPoints()
-	QuestLogTitleText:SetPoint("TOP", QuestLogFrame, "TOP", 0, -18)
-
-	-- Move the detail frame to the right and stretch it to full height
-	QuestLogDetailScrollFrame:ClearAllPoints()
-	QuestLogDetailScrollFrame:SetPoint("TOPLEFT", QuestLogListScrollFrame, "TOPRIGHT", 31, 1)
-	QuestLogDetailScrollFrame:SetHeight(336)
-
-	-- Expand the quest list to full height
-	QuestLogListScrollFrame:SetHeight(336)
-
-	-- Create additional quest rows
-	local oldQuestsDisplayed = QUESTS_DISPLAYED
-	_G.QUESTS_DISPLAYED = _G.QUESTS_DISPLAYED + 16
-	for i = oldQuestsDisplayed + 1, QUESTS_DISPLAYED do
-		local button = CreateFrame("Button", "QuestLogTitle"..i, QuestLogFrame, "QuestLogTitleButtonTemplate")
-		button:SetID(i)
-		button:Hide()
-		button:ClearAllPoints()
-		button:SetPoint("TOPLEFT", _G["QuestLogTitle"..(i-1)], "BOTTOMLEFT", 0, 1)
-	end
-
-	if not C.db["Skins"]["BlizzardSkins"] then
-		-- Get quest frame textures
-		local regions = {QuestLogFrame:GetRegions()}
-		-- Set top left texture
-		regions[3]:SetTexture("Interface\\QUESTFRAME\\UI-QuestLogDualPane-Left")
-		regions[3]:SetSize(512, 512)
-		-- Set top right texture
-		regions[4]:ClearAllPoints()
-		regions[4]:SetPoint("TOPLEFT", regions[3], "TOPRIGHT", 0, 0)
-		regions[4]:SetTexture("Interface\\QUESTFRAME\\UI-QuestLogDualPane-Right")
-		regions[4]:SetSize(256, 512)
-		-- Hide bottom left and bottom right textures
-		regions[5]:Hide()
-		regions[6]:Hide()
-	end
-
-	-- Position and resize abandon button
-	QuestLogFrameAbandonButton:SetSize(100, 22)
-	QuestLogFrameAbandonButton:SetText(ABANDON_QUEST_ABBREV)
-	QuestLogFrameAbandonButton:ClearAllPoints()
-	QuestLogFrameAbandonButton:SetPoint("BOTTOMLEFT", QuestLogFrame, "BOTTOMLEFT", 17, 52)
-
-	-- Position and resize share button
-	QuestFramePushQuestButton:SetSize(100, 22)
-	QuestFramePushQuestButton:SetText(SHARE_QUEST_ABBREV)
-	QuestFramePushQuestButton:ClearAllPoints()
-	QuestFramePushQuestButton:SetPoint("LEFT", QuestLogFrameAbandonButton, "RIGHT", 3, 0)
-
-	-- Add map button
-	local logMapButton = CreateFrame("Button", nil, QuestLogFrame, "UIPanelButtonTemplate")
-	logMapButton:SetText(BRAWL_TOOLTIP_MAP)
-	logMapButton:ClearAllPoints()
-	logMapButton:SetPoint("LEFT", QuestFramePushQuestButton, "RIGHT", 3, 0)
-	logMapButton:SetSize(100, 22)
-	logMapButton:SetScript("OnClick", ToggleWorldMap)
-	if C.db["Skins"]["BlizzardSkins"] then B.Reskin(logMapButton) end
-
-	-- Position and size close button
-	QuestFrameExitButton:SetSize(80, 22)
-	QuestFrameExitButton:SetText(CLOSE)
-	QuestFrameExitButton:ClearAllPoints()
-	QuestFrameExitButton:SetPoint("BOTTOMRIGHT", QuestLogFrame, "BOTTOMRIGHT", -42, 52)
-
-	-- Empty quest frame
-	QuestLogNoQuestsText:ClearAllPoints()
-	QuestLogNoQuestsText:SetPoint("TOP", QuestLogListScrollFrame, 0, -50)
-	hooksecurefunc(EmptyQuestLogFrame, "Show", function()
-		EmptyQuestLogFrame:ClearAllPoints()
-		EmptyQuestLogFrame:SetPoint("BOTTOMLEFT", QuestLogFrame, "BOTTOMLEFT", 20, -76)
-		EmptyQuestLogFrame:SetHeight(487)
-	end)
-
-	-- Move ClassicCodex
-	if CodexQuest then
-		local buttonShow = CodexQuest.buttonShow
-		buttonShow:SetWidth(55)
-		buttonShow:SetText(DB.InfoColor..SHOW)
-
-		local buttonHide = CodexQuest.buttonHide
-		buttonHide:ClearAllPoints()
-		buttonHide:SetPoint("LEFT", buttonShow, "RIGHT", 5, 0)
-		buttonHide:SetWidth(55)
-		buttonHide:SetText(DB.InfoColor..HIDE)
-
-		local buttonReset = CodexQuest.buttonReset
-		buttonReset:ClearAllPoints()
-		buttonReset:SetPoint("LEFT", buttonHide, "RIGHT", 5, 0)
-		buttonReset:SetWidth(55)
-		buttonReset:SetText(DB.InfoColor..RESET)
-	end
-end
-
 function S:QuestLogLevel()
 	local numEntries = GetNumQuestLogEntries()
 
@@ -353,7 +247,6 @@ function S:QuestTracker()
 
 	if not C.db["Skins"]["QuestTracker"] then return end
 
-	S:EnhancedQuestLog()
 	S:EnhancedQuestTracker()
 	hooksecurefunc("QuestLog_Update", S.QuestLogLevel)
 end
